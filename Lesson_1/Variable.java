a478b64ebc5a9e1e0a0a918caf1a3e52e16a3e2b
Vladimir_Lukyanov <vladimirsokolov274@gmail.com>
2021-04-02 11:07:21 +0300

    implement HW_1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -1,30 +1,23 @@
 public class Variable {
     public static void main(String[] args) {
-        // количество ядер процессора
-        byte proCores = 12;
-        //    максимальная частота процессора  
+        byte cpuCores = 12;
         short maxFreqCpu = 3300;
-        //  оперативаная память  в гигабайтах
-        int ram =32;        
-        //  объе жесткого диска в гигабайтах
-        long rom = 512;
-        // использованно  гигабайт на жестком диске 
-        float useRom = 21.42f;
-        // использованно гигабайт оперативной памяти
-        double useRam = 3.04d;
-        // знак Java
-        char cupJava ='\u2615';
-        // знак сердечко
-        char heart = '\u2665';
-        //Изучаю ли я Java ?
-        boolean javaLearn = true;
-        System.out.println("Количество ядер моего процессора = " + proCores);
+        int ramGb =32;
+        long romGb = 512;
+        float useRomGb = 21.42f;
+        double useRamGb = 3.04d;
+        char signCupJava ='\u2615';
+        char signHeart = '\u2665';
+        // Изучаю ли я Java ?
+        boolean learnerJava = true;
+
+        System.out.println("Количество ядер моего процессора = " + cpuCores);
         System.out.println("Максимальная частота моего процессора в MHz = " + maxFreqCpu);
-        System.out.println("Количество оперативной памяти моего пк в гигабайтах = " + ram);
-        System.out.println("Объем жесткого диска моего пк в гигабайтах = " + rom);
-        System.out.println("использованно гигабайт на жестком диске = " + useRom);
-        System.out.println("использованно гигабайт оперативной памяти = " + useRam);
-        System.out.println("Мне нравится изучать  Java " + cupJava + " " + heart);
-        System.out.println("Я изучаю Java = " + javaLearn);
+        System.out.println("Количество оперативной памяти моего пк в гигабайтах = " + ramGb);
+        System.out.println("Объем жесткого диска моего пк в гигабайтах = " + romGb);
+        System.out.println("использованно гигабайт на жестком диске = " + useRomGb);
+        System.out.println("использованно гигабайт оперативной памяти = " + useRamGb);
+        System.out.println("Мне нравится изучать  Java " + signCupJava + " " + signHeart);
+        System.out.println("Я изучаю Java = " + learnerJava);
     }
 }
